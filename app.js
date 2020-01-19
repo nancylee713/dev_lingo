47ed146628c107f9c755fc0198556b792c9bd3dc
nancylee713 <srlee713@gmail.com>
2020-01-18 21:33:29 -0700

    Add body parser to app

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRouter = require('./routes/index');
 var vocabRouter = require('./routes/api/v1/vocab');
 
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/vocab', vocabRouter);
